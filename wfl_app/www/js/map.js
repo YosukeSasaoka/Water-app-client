120b1076a66d102e48ef8c645c34f69930baf937
usa <ferutjp@gmail.com>
2017-02-24 01:25:57 +0900

    map.html,map.js修正

--- wfl_app/www/js/map.js
+++ wfl_app/www/js/map.js
@@ -1,88 +1,46 @@
-var marker = [];
+var map;
 
-var image = {
-url : 'images/map/marker.png',
-scaledSize : new google.maps.Size(34, 50)
-// ↑ここで画像のサイズを指定
-}
+document.addEventListener("deviceready", function() {
+  var div = document.getElementById("map_canvas");
 
-var infoWindow = [];
+  // Initialize the map view
+  map = plugin.google.maps.Map.getMap(div);
+
+  var center = new plugin.google.maps.LatLng(33.615562, 133.337790);
+
+  var data = [ // マーカーを立てる場所名・緯度・経度
+    {'title':'にこ淵','position':new plugin.google.maps.LatLng(33.705816,133.341167)},
+    {'title':'大樽の滝','position':new plugin.google.maps.LatLng(33.521375,133.242819)},
+    {'title':'瀬戸川渓谷','position':new plugin.google.maps.LatLng(33.737271,133.403390)}
+  ];
 
 
 
-var markerData = [ // マーカーを立てる場所名・緯度・経度
-    {
-        name: 'にこ淵',
-        lat: 33.705816, 
-        lng: 133.341167,
-        icon: image 
-    }, {
-        name: '大樽の滝',
-        lat: 33.521375, 
-        lng: 133.242819,
-        icon: image 
-    }, {
-        name: '瀬戸川渓谷',
-        lat: 33.737271, 
-        lng: 133.403390,
-        icon: image 
+  // Wait u ntil the map is ready status.
+  map.addEventListener(plugin.google.maps.event.MAP_READY, function(){
+
+    map.animateCamera({
+  'target': center,
+  'zoom': 11
+  });
+
+    //marker create
+    addMarkers(data,function(markers) {
+      markers[markers.length -1].showInfoWindow();
+    });
+    function addMarkers(data, callback){
+      var markers = [];
+      function onMarkerAdded(marker){
+        markers.push(marker);
+        if(markers.length === data.length){
+          callback(markers);
+        }
+      }
+      data.forEach(function(markerOptions){
+        map.addMarker(markerOptions,onMarkerAdded);
+      })
     }
-];
 
-window.onload =function() {
-  if (navigator.geolocation) {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-		  //var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-		  var latlng = new google.maps.LatLng(33.615562, 133.337790);
-		  var options = {
-			  zoom: 11,
-			  center: latlng,
-			  mapTypeId: google.maps.MapTypeId.ROADMAP
-		  };
-		  var map = new google.maps.Map(document.getElementById('map_canvas'), options);
-		  /*var marker = new google.maps.Marker({
-			  position: latlng,
-			  map: map,
-			  title: '現在地'
-		  });*/
-		  
-		  
-		// マーカー毎の処理
-			for (var i = 0; i < markerData.length; i++) {
-				markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
-				marker[i] = new google.maps.Marker({ // マーカーの追加
-					position: markerLatLng, // マーカーを立てる位置を指定
-					map: map, // マーカーを立てる地図を指定
-					icon: image
-				});
 
-				infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
-					content: '<a href="taki' + i + '.html"><div class="sample">' + markerData[i]['name'] + '</div></a>' // 吹き出しに表示する内容
-				});
-				
-				markerEvent(i); // マーカーにクリックイベントを追加
-			}
-
-			function markerEvent(i) {
-				var openFlag = [0];
-				marker[i].addListener('click', function(){
-					var id=this.get("id");
-					if(openFlag[id]==0){
-						infoWindow[i].open(map, this);
-						openFlag[id]=1;
-					}else{
-						infoWindow[i].close();
-						openFlag[id]=0;
-					};
-				})
-			};
-		  
-		  
-		  
-	  }, function(e) {
-		  document.getElementById('message').innerHTML = typeof e == 'string' ? e : e.message;
-	  });
-  } else {
-	  document.getElementById('message').innerHTML = 'Location APIがサポートされていません。';
-  }
-};
+  },false);
+});
