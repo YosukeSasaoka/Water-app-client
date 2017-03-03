709a26e81a3df8bfa7cf3b856757cb679c6b2608
usa <ferutjp@gmail.com>
2017-03-03 22:59:44 +0900

    timer連動、CSS微調整

--- wfl_app/www/js/map.js
+++ wfl_app/www/js/map.js
@@ -9,7 +9,7 @@ document.addEventListener("deviceready", function() {
   var center = new plugin.google.maps.LatLng(33.615562, 133.337790);
 
   var data = [ // マーカーを立てる場所名・緯度・経度
-    {'title':'にこ淵','position':new plugin.google.maps.LatLng(33.705816,133.341167)},
+    {'title':'<b>にこ淵</b>','position':new plugin.google.maps.LatLng(33.705816,133.341167)},
     {'title':'大樽の滝','position':new plugin.google.maps.LatLng(33.521375,133.242819)},
     {'title':'瀬戸川渓谷','position':new plugin.google.maps.LatLng(33.737271,133.403390)}
   ];
@@ -24,7 +24,37 @@ document.addEventListener("deviceready", function() {
   'zoom': 11
   });
 
-    //marker create
+
+  var infoWindow = new plugin.google.maps.InfoWindow();
+
+map.addMarker({
+  position: {lat: 33.705816, lng: 133.34116},
+  draggable: true,
+  title: [
+    '<a href="taki0.html">にこ渕</a>',
+  ].join(""),
+  infoWindow: infoWindow
+});
+
+map.addMarker({
+  position: {lat: 33.521375, lng: 133.242819},
+  draggable: true,
+  title: [
+    '<a href="taki1.html">大樽の滝</a>',
+  ].join(""),
+  infoWindow: infoWindow
+});
+
+map.addMarker({
+  position: {lat: 33.737271, lng: 133.403390},
+  draggable: true,
+  title: [
+    '<a href="taki2.html">瀬戸川渓谷</a>',
+  ].join(""),
+  infoWindow: infoWindow
+});
+
+    /*marker create
     addMarkers(data,function(markers) {
       markers[markers.length -1].showInfoWindow();
     });
@@ -40,7 +70,7 @@ document.addEventListener("deviceready", function() {
         map.addMarker(markerOptions,onMarkerAdded);
       })
     }
-
+    */
 
   },false);
 });
